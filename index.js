b38f60003db355a73a14a32db60d25eb08054d1d
Martin Georgiu <login@georgiu.cz>
2019-12-30 03:01:22 +0100

    rollback

--- index.js
+++ index.js
@@ -9,35 +9,39 @@ server.listen(process.env.LOCAL_PORT)
 
 app.use(express.static(__dirname + '/public'))
 
-app.use(
-	session({
-		secret: 'secret',
-		resave: true,
-		saveUninitialized: true,
-	}),
-)
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+// app.use(
+// 	session({
+// 		secret: 'secret',
+// 		resave: true,
+// 		saveUninitialized: true,
+// 	}),
+// )
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+
+// app.get('/', function(request, response) {
+// 	response.sendFile(path.join(__dirname + '/public/login.html'))
+// })
+// app.post('/auth', function(request, response) {
+// 	var username = request.body.username
+// 	var password = request.body.password
+// 	if (username == process.env.USERNAME && password == process.env.PASSWORD) {
+// 		request.session.loggedin = true
+// 		response.redirect('/home')
+// 	} else {
+// 		response.send('Please enter Username and Password!')
+// 	}
+// })
+// app.get('/home', function(request, response) {
+// 	if (request.session.loggedin) {
+// 		response.sendFile(__dirname + '/public/index.html')
+// 	} else {
+// 		response.send('Please login to view this page!')
+// 	}
+// })
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/public/login.html'))
-})
-app.post('/auth', function(request, response) {
-	var username = request.body.username
-	var password = request.body.password
-	if (username == process.env.USERNAME && password == process.env.PASSWORD) {
-		request.session.loggedin = true
-		response.redirect('/home')
-	} else {
-		response.send('Please enter Username and Password!')
-	}
-})
-app.get('/home', function(request, response) {
-	if (request.session.loggedin) {
-		response.sendFile(__dirname + '/public/index.html')
-	} else {
-		response.send('Please login to view this page!')
-	}
+	response.sendFile(__dirname + '/public/index.html')
 })
 
 io.on('connection', function(socket) {
