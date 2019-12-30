6072940a181046fd1e8e3b9c320819c76a331bca
Martin Georgiu <login@georgiu.cz>
2019-12-30 02:55:30 +0100

    security updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-var server = require('http').Server(app)
-var io = require('socket.io')(server)
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 require('dotenv').config()
 
@@ -9,7 +9,36 @@ server.listen(process.env.LOCAL_PORT)
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
+app.use(
+	session({
+		secret: 'secret',
+		resave: true,
+		saveUninitialized: true,
+	}),
+)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/public/login.html'))
+})
+app.post('/auth', function(request, response) {
+	var username = request.body.username
+	var password = request.body.password
+	if (username == process.env.USERNAME && password == process.env.PASSWORD) {
+		request.session.loggedin = true
+		response.redirect('/home')
+	} else {
+		response.send('Please enter Username and Password!')
+	}
+})
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.sendFile(__dirname + '/public/index.html')
+	} else {
+		response.send('Please login to view this page!')
+	}
+})
 
 io.on('connection', function(socket) {
 	socket.on('send-data', function(data) {
