f5f0ee649adb5b94b61227e4fd3b09bf3583cbb4
Martin Georgiu <login@georgiu.cz>
2019-12-31 14:33:37 +0100

    login support

--- index.js
+++ index.js
@@ -10,36 +10,28 @@ server.listen(process.env.LOCAL_PORT)
 
 app.use(express.static(__dirname + '/public'))
 
-// app.use(
-// 	session({
-// 		secret: 'secret',
-// 		resave: true,
-// 		saveUninitialized: true,
-// 	}),
-// )
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(bodyParser.json())
+app.use(
+	session({
+		secret: 'secret',
+		resave: true,
+		saveUninitialized: true,
+	}),
+)
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// app.get('/', function(request, response) {
-// 	response.sendFile(path.join(__dirname + '/public/login.html'))
-// })
-// app.post('/auth', function(request, response) {
-// 	var username = request.body.username
-// 	var password = request.body.password
-// 	if (username == process.env.USERNAME && password == process.env.PASSWORD) {
-// 		request.session.loggedin = true
-// 		response.redirect('/home')
-// 	} else {
-// 		response.send('Please enter Username and Password!')
-// 	}
-// })
-// app.get('/home', function(request, response) {
-// 	if (request.session.loggedin) {
-// 		response.sendFile(__dirname + '/public/index.html')
-// 	} else {
-// 		response.send('Please login to view this page!')
-// 	}
-// })
+app.post('/auth', function(request, response) {
+	var username = request.body.username
+	var password = request.body.password
+	if (username == process.env.USERNAME && password == process.env.PASSWORD) {
+		request.session.loggedin = true
+		response.redirect('/')
+	} else {
+		response.send('Please enter Username and Password!')
+	}
+})
+app.get('/login', function(request, response) {
+	response.sendFile(__dirname + '/public/login.html')
+})
 
 app.get('/', function(request, response) {
 	response.sendFile(__dirname + '/public/index.html')
