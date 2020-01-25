ba2ff44f340f28c4ec4a10faab22fc018cc421c1
Martin Georgiu <login@georgiu.cz>
2020-01-25 10:51:33 +0100

    add auth #21

--- public/script.js
+++ public/script.js
@@ -1,29 +1,28 @@
-let socket = io({
-	query: {
-		token: 123,
-	},
-})
 fetch('/get-key', { method: 'POST' })
 	.then(response => response.json())
 	.then(data => {
-		console.log('Success:', data)
-	})
-let toggle = {
-	mainLight: document.getElementById('mainLightToggle'),
-	tableLight: document.getElementById('tableLightToggle'),
-}
-socket.emit('request-data')
-socket.on('send-data', function(data) {
-	for (const key in toggle) {
-		if (toggle.hasOwnProperty(key)) {
-			toggle[key].checked = data[key]
+		let socket = io({
+			query: {
+				token: data.token,
+			},
+		})
+		let toggle = {
+			mainLight: document.getElementById('mainLightToggle'),
+			tableLight: document.getElementById('tableLightToggle'),
 		}
-	}
-})
+		socket.emit('request-data')
+		socket.on('send-data', function(data) {
+			for (const key in toggle) {
+				if (toggle.hasOwnProperty(key)) {
+					toggle[key].checked = data[key]
+				}
+			}
+		})
 
-toggle.mainLight.addEventListener('click', () => {
-	socket.emit('toggle', { type: 'mainLight', value: toggle.mainLight.checked })
-})
-toggle.tableLight.addEventListener('click', () => {
-	socket.emit('toggle', { type: 'tableLight', value: toggle.tableLight.checked })
-})
+		toggle.mainLight.addEventListener('click', () => {
+			socket.emit('toggle', { type: 'mainLight', value: toggle.mainLight.checked })
+		})
+		toggle.tableLight.addEventListener('click', () => {
+			socket.emit('toggle', { type: 'tableLight', value: toggle.tableLight.checked })
+		})
+	})
